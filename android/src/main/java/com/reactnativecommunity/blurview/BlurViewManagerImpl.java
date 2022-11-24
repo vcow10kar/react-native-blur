6dc36c45bbe2d1a0caf4fdaf02ca0d9d0b927379
Vaishnavi Kawthankar <vkawthankar@gmail.com>
2022-11-25 01:26:40 +0530

    Updated to have this work with navigation

--- android/src/main/java/com/reactnativecommunity/blurview/BlurViewManagerImpl.java
+++ android/src/main/java/com/reactnativecommunity/blurview/BlurViewManagerImpl.java
@@ -7,7 +7,7 @@ import android.view.ViewGroup;
 import com.facebook.react.uimanager.ThemedReactContext;
 
 import eightbitlab.com.blurview.BlurView;
-import eightbitlab.com.blurview.RenderEffectBlur;
+// import eightbitlab.com.blurview.RenderEffectBlur;
 import eightbitlab.com.blurview.RenderScriptBlur;
 
 import java.util.Objects;
@@ -29,17 +29,10 @@ class BlurViewManagerImpl {
       .getDecorView();
     ViewGroup rootView = decorView.findViewById(android.R.id.content);
     Drawable windowBackground = decorView.getBackground();
-    if (Build.VERSION.SDK_INT >= 31) {
-      blurView
-        .setupWith(rootView, new RenderEffectBlur())
-        .setFrameClearDrawable(windowBackground)
-        .setBlurRadius(defaultRadius);
-    } else {
-      blurView
-        .setupWith(rootView, new RenderScriptBlur(ctx))
-        .setFrameClearDrawable(windowBackground)
-        .setBlurRadius(defaultRadius);
-    }
+    blurView
+      .setupWith(rootView, new RenderScriptBlur(ctx))
+      .setFrameClearDrawable(windowBackground)
+      .setBlurRadius(defaultRadius);
     return blurView;
   }
 
